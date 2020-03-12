675a7972d0b94912ce40269f14db03274b9dae2e
LinaFawn <52353680+LinaFawn@users.noreply.github.com>
2020-03-12 09:43:42 +0300

    Add files via upload

---
+++ Featers.java
@@ -0,0 +1,18 @@
+public class Featers {
+    public static void Featers(){
+
+        System.out.println("Wow");
+        Wow();
+    }
+
+    public static void Wow(){
+        System.out.println("lol");
+        int a =1;
+        int b = 3;
+        System.out.println(sum(a, b));
+
+    }
+    public static int sum(int a, int b){
+        return a+b;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Пустой");
+
+        System.out.println("hotfix");
+
+        Featers.Featers();
+        System.out.println("My Second Feat");
+        short a;
+
+    }
+}
+
